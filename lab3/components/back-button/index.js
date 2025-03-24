b94697abd96364eafa75027762597c74c1c8936b
Vlastelin <broforce.play@gmail.com>
2025-03-24 10:30:31 +0300

    fix

--- lab3/components/back-button/index.js
+++ lab3/components/back-button/index.js
@@ -10,7 +10,6 @@ export class BackButtonComponent {
 	getHTML() {
 		return `
     <button id="back-button" class="btn-custom btn-primary">
-        <i></i>
         Назад
     </button>`
 	}
--- lab3/components/blueprint-card/index.js
+++ lab3/components/blueprint-card/index.js
@@ -14,7 +14,6 @@ export class BlueprintCardComponent {
         <div class="card-body-custom">
             <h5 class="card-title-custom">${data.title}</h5>
             <p class="card-text-custom">
-                <i></i> 
                 Элементов: ${data.elements.length}
             </p>
             ${this.carousel.getHTML(data)}
